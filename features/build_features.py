950aa50cbf8c88b732f61b604844204ee0688eec
Debojeet Das <debojeetdas218@gmail.com>
2026-05-04 01:59:33 +0530

    Fn to Create processed data dir added

--- features/build_features.py
+++ features/build_features.py
@@ -5,3 +5,8 @@ from glob import glob
 
 RAW_DATA_PATH = "data/raw"
 PROCESSED_DATA_PATH = "data/processed"
+
+def create_processed_path():
+    if not os.path.exists(PROCESSED_DATA_PATH):
+        os.makedirs(PROCESSED_DATA_PATH)
+
