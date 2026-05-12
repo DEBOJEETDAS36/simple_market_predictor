165d3b580af2edd111458f511681e8bd7d0979ab
Debojeet Das <debojeetdas218@gmail.com>
2026-05-13 03:12:38 +0530

    Fn added in train models to create target

--- models/train_models.py
+++ models/train_models.py
@@ -1,3 +1,30 @@
 import pandas as pd
 import os
 from glob import glob
+from sklearn.metrics import accuracy_score, classification_report
+from xgboost import XGBClassifier
+import joblib
+
+PROCESSED_DATA_PATH = "data/processed"
+MODEL_SAVE_PATH = "saved_models"
+
+FEATURE_COLUMNS = [
+    "returns",
+    "sma_10",
+    "sma_50",
+    "volatility",
+    "rsi",
+    "momentum"
+]
+
+def create_target(df):
+    """
+    Create binary target:
+    1 = next day price up
+    0 = next day price down
+    """
+    df["future_return"] = df["close"].shift(-1) - df["close"]
+    df["target"] = (df["future_return"] > 0).astype(int)
+
+    return df
+
